308f1e569fee5c51d0bf23a0f8b0a9b44c7b8c37
артур <okolobaxha@yandex.ru>
2024-07-21 17:34:36 +0700

    1

--- index.ts
+++ index.ts
@@ -1,67 +1,31 @@
-// import { Ability, EventsSDK, LocalPlayer, Hero, Entity } from "github.com/octarine-public/wrapper/index"
-// import { MenuManager } from "./menu"
+import { Ability, Hero, ImageData, LocalPlayer, Menu } from "github.com/octarine-public/wrapper"
 
-// const bootstarp = new (class COctarineTest {
-// 	private readonly menu = new MenuManager()
+const Entry: Menu.Node = Menu.AddEntry("Utility")
+const OctarineTest: Menu.Node = Entry.AddNode("Octarine Test", ImageData.Paths.Icons.arrow_gold_dif)
+const TestKey: Menu.KeyBind = OctarineTest.AddKeybind("Test key")
 
-// 	constructor() {
-// 		this.menu.RushKey.OnPressed(() => this.pressedRush())
-// 	}
-
-// 	public EntityDestroyed(_: Entity) {
-// 		console.log("Entity destroyed!")
-// 	}
-
-// 	private pressedRush(): void {
-// 		const hero = this.getLocalHero()
-// 		if (hero === undefined) {
-// 			return
-// 		}
-		
-// 		hero.Spells.forEach((spell: Nullable<Ability>) => {
-// 			console.log(spell)
-// 		})
-// 	}
-
-// 	private getLocalHero(): Nullable<Hero> {
-// 		console.log("trying to get a hero")
-
-// 		const hero = LocalPlayer?.Hero
-// 		if (hero === undefined || !hero.IsAlive) {
-// 			return
-// 		}
-// 		if (hero.IsInvulnerable || hero.IsCharge) {
-// 			return
-// 		}
-// 		return hero
-// 	}
-
-// })
-
-// EventsSDK.on("EntityDestroyed", entity => bootstarp.EntityDestroyed(entity))
-
-
-import {
-	Entity,
-	EventsSDK,
-	Hero,
-	Input,
-	LocalPlayer,
-	Sleeper,
-	Unit
-} from "github.com/octarine-public/wrapper/index"
-
-import { MenuManager } from "./menu"
-
-const bootstrap = new (class CAggroDeaggro {
-	private readonly units: Unit[] = []
-	private readonly menu = new MenuManager()
-
-	constructor() {
-		this.menu.AggroKey.OnPressed(() => this.pressedAggro())
-	}
-
-	private pressedAggro() {
+const GetLocalHero = (): Nullable<Hero> => {
+	const hero = LocalPlayer?.Hero
+	if (hero === undefined || !hero.IsAlive) {
 		return
 	}
-})()
+	if (hero.IsInvulnerable || hero.IsCharge) {
+		return
+	}
+	return hero
+}
+
+TestKey.OnPressed(() => {
+	const hero: Nullable<Hero> = GetLocalHero()
+	if (hero === undefined) {
+		return
+	}
+
+	const spells: Nullable<Ability>[] = hero.Spells;
+
+	for (const spell of spells) {
+		if (spell) {
+			spell.UseAbility()
+		}
+	}
+})
