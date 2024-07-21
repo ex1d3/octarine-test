7c3770de85f725c6707dcbbbb5cc4ccfe7721122
артур <okolobaxha@yandex.ru>
2024-07-21 16:32:34 +0700

    1

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { EventsSDK, LocalPlayer } from "github.com/octarine-public/wrapper/index"
 
 EventsSDK.on("GameStarted", () => {
-	console.log(LocalPlayer?.Hero)
+	console.log(LocalPlayer)
 })
