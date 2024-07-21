ab570ac7651ba551820eb1bbf3e88e19f4c13d87
артур <okolobaxha@yandex.ru>
2024-07-21 16:50:56 +0700

    1

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-import { EventsSDK, LocalPlayer } from "github.com/octarine-public/wrapper/index"
+import { Ability, EventsSDK, LocalPlayer } from "github.com/octarine-public/wrapper/index"
 
 EventsSDK.on("AttackStarted", () => {
-	console.log(LocalPlayer?.Hero?.Spells[0]?.UseAbility())
+	
+	LocalPlayer?.Hero?.Spells.forEach((spell: Nullable<Ability>) => {
+		spell?.UseAbility()
+	})
 })
