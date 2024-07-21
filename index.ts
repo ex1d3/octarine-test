07d467e602dc35ab164dd61ccf755e53060f708b
артур <okolobaxha@yandex.ru>
2024-07-21 17:51:13 +0700

    1

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import {
+	Ability,
 	EventsSDK,
+	Hero,
+	LocalPlayer,
 	Menu,
 	ParticlesSDK,
 	TickSleeper,
@@ -19,13 +22,28 @@ const OctarineTest = Entry.AddNode(
 const RushState = OctarineTest.AddToggle("State")
 const RushKey = OctarineTest.AddKeybind("Rush key")
 
+const getLocalHero = (): Nullable<Hero> => {
+	const hero: Nullable<Hero> = LocalPlayer?.Hero
+	if (hero === undefined || !hero.IsAlive) {
+		return
+	}
+	return hero
+}
+
 RushKey.OnPressed(() => {
 	if (!RushState.value) {
 		console.log("Rush key pressed, but its offed!")
 		return
 	}
 
-	console.log("Rush key pressed!")
+	const hero: Nullable<Hero> = getLocalHero()
+	if (hero === undefined) {
+		return
+	}
+
+	hero.Spells.forEach((spell: Nullable<Ability>): void => {
+		spell?.UseAbility()
+	})
 })
 
 EventsSDK.on("GameEnded", () => {
