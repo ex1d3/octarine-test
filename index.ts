8d59520c7ea268aa24677a4aefd043dc96f5d4a2
артур <okolobaxha@yandex.ru>
2024-07-21 16:14:30 +0700

    1

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { AddEntry } from "github.com/octarine-public/wrapper/wrapper/Menu"
 
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
+	AddEntry("12")
 })
